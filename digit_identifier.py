f00264130c50a46308db55823072f79200fbf329
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-04-17 13:46:45 -0400

    Binary normalization

--- digit_identifier.py
+++ digit_identifier.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from pathlib import Path
 from sys import argv
 
@@ -19,17 +20,38 @@ def get_digit_from_file_name(img_file_name):
 	return int(split_name[0])
 
 
-def create_database(image_dir, loading_flag):
+def create_database(image_dir, normalization=None):
 	database = dict()
 	for i in range(10): # Digits from 0 to 9
 		database[i] = list()
 
 	for img_file in image_dir.glob(_PATTERN_DATASET_IMG):
 		digit =  get_digit_from_file_name(img_file.name)
-		digit_image = cv2.imread(img_file, loading_flag)
+		digit_image = cv2.imread(img_file, cv2.IMREAD_COLOR)
+
+		if normalization:
+			digit_image = normalization(digit_image)
+
 		database[digit].append(digit_image)
 
 	return database
+
+
+def normalize_binary(image, threshold_factor):
+	threshold = threshold_factor * 255
+	shap = image.shape[:2]
+	size0, size1 = shap
+
+	normalized_image = np.zeros(shap)
+	for i0 in range(size0):
+		for i1 in range(size1):
+			bgr = image[i0, i1]
+			pixel_sum = sum((int(n) for n in bgr))
+
+			if pixel_sum / 3 >= threshold:
+				normalized_image[i0, i1] = 1
+
+	return normalized_image
 
 
 def print_dict(a_dict):
@@ -59,9 +81,11 @@ def recognize_digit(digit_image, database):
 
 digit_path = argv[1]
 
-db = create_database(Path.cwd()/"dataset", cv2.IMREAD_GRAYSCALE)
+norm_bin = lambda img: normalize_binary(img, 0.5)
+db = create_database(Path.cwd()/"dataset", norm_bin)
 
-image = cv2.imread(digit_path, cv2.IMREAD_GRAYSCALE)
+image = cv2.imread(digit_path, cv2.IMREAD_COLOR)
+image = norm_bin(image)
 digit, confidence = recognize_digit(image, db)
 
 print(f"Digit: {digit}\nConfidence: {confidence}")
